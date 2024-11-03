5fceb2e2de5b475ac3598ed7fdcc32eb2288fb26
adityamahla <adityamahla@gmail.com>
2024-11-03 20:55:05 +1100

    Tailwind CSS Setup and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-white ">DevOverflow Application</h1>
+  <h1 className="background-light850_dark100 text-3xl font-black text-primary-500">
+    DevOverflow Application
+  </h1>
 );
 
 export default Home;
