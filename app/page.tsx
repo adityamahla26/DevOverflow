5d52a31653f06b7c0ef02383acd9cce31724c185
adityamahla <adityamahla@gmail.com>
2024-11-04 10:29:02 +1100

    Setup Fonts, Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 const Home = () => (
-  <h1 className="background-light850_dark100 text-3xl font-black text-primary-500">
-    DevOverflow Application
-  </h1>
+  <>
+    <h1 className="background-light850_dark100 text-3xl font-black text-primary-500">
+      DevOverflow Application
+    </h1>
+    <h1 className="background-light850_dark100 font-spaceGrotesk text-3xl font-black text-primary-500">
+      DevOverflow Application (INTER)
+    </h1>
+  </>
 );
 
 export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,10 @@ const config: Config = {
       screens: {
         xs: "420px",
       },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        spaceGrotesk: ["var(--font-space-grotesk)"],
+      },
     },
   },
   plugins: [],
