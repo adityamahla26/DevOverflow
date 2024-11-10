3faabd24017e29addc6988cb1a7714333795b5dc
adityamahla <adityamahla@gmail.com>
2024-11-10 18:32:20 +1100

    Setup NavLinks and Navigation for Mobile

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -7,18 +7,18 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import ROUTES from "@/constants/routes";
 
+import NavLinks from "./NavLinks";
+
 const MobileNavigation = () => {
   return (
     <>
       <Sheet>
-        <SheetTrigger>
+        <SheetTrigger asChild>
           <Image
             src="/icons/hamburger.svg"
             width={36}
@@ -32,16 +32,23 @@ const MobileNavigation = () => {
           side="left"
         >
           <SheetTitle className="hidden">Navigation</SheetTitle>
+
           <Link href="/" className="flex items-center gap-1">
-            <Image src="/images/site-logo.svg" width={23} height={23} alt="" />
+            <Image
+              src="/images/site-logo.svg"
+              width={23}
+              height={23}
+              alt="Logo"
+            />
             <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
               Dev<span className="text-primary-500">Overflow</span>
             </p>
           </Link>
+
           <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="h-full flex-col gap-6 pt-16">
-                <p>Nav Links</p>
+              <section className="flex h-full flex-col gap-6 pt-16">
+                <NavLinks isMobileNav />
               </section>
             </SheetClose>
             <div className="flex flex-col gap-3">
@@ -55,7 +62,7 @@ const MobileNavigation = () => {
               <SheetClose asChild>
                 <Link href={ROUTES.SIGN_UP}>
                   <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-                    <span>Sign Up</span>
+                    Sign Up
                   </Button>
                 </Link>
               </SheetClose>
